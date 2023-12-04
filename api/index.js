a346bfdf7a854cbe53a55c70b43379eebe108648
nawadeveloper <nawarajusa@gmail.com>
2023-12-03 21:46:43 -0600

    minor changes

--- api/index.js
+++ api/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
 const cors = require("cors");
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("hello again!");
 });
 
-app.listen("4000");
+app.listen(process.env.PORT);
