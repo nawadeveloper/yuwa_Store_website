dfbd9886b9c7c19e6843add3a37ed67d9066c5c9
nawadeveloper <nawarajusa@gmail.com>
2023-12-05 23:29:29 -0600

    bcrypt added

--- api/Controllers/AuthController.js
+++ api/Controllers/AuthController.js
@@ -1,27 +1,32 @@
 const Users = require("../Models/Users");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const sendMessage = (req, res) => {
   res.json({ message: "auth controller message." });
 };
 
-const register = async (req, res) => {
+const register = (req, res) => {
   const { username, password } = req.body;
-  if (username === "" || password === "") {
+  if (username.trim() === "" || password.trim() === "") {
     res.status(400).json({
       message: "Please enter all required filled.",
     });
     return;
   }
 
-  try {
-    const addUser = await Users.create({
-      username,
-      password,
-    });
-    res.json({ message: `${addUser?.username} is created.` });
-  } catch (e) {
-    res.status(400).json({ message: e });
-  }
+  bcrypt.hash(password, saltRounds, async (err, hash) => {
+    // Store hash in your password DB.
+    try {
+      const addUser = await Users.create({
+        username,
+        password: hash,
+      });
+      res.json({ message: `${addUser?.username} is created.` });
+    } catch (e) {
+      res.status(400).json({ message: e.message });
+    }
+  });
 };
 
 module.exports = {
